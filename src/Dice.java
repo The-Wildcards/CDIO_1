96759702c0e9ae3b49f1255a657f56479c203266
The-Wildcards <adrian.macauley1207@gmail.com>
2023-09-24 14:12:21 +0200

    Dice class og prepr til exp 1.
    - Added roll dice function
    - Created class for expansion pack 1.

---
+++ src/Dice.java
@@ -0,0 +1,18 @@
+public class Dice {
+    
+    int rollValue;
+    //Dice constructer
+    public Dice (int rollValue){
+        this.rollValue = rollValue;
+    }
+    // Rolling the actual dice
+    public int rollDice(){
+        //Using math.random to generate random number between 1 and 6.
+        rollValue = (int) ((Math.random() * (6 - 1)) + 1) ;
+        return rollValue;
+    }
+
+    public int getRollValue(){
+        return rollValue;
+    }
+}
