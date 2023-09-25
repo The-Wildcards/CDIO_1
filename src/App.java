0afdaaaa601c66de2a5f2df7642a9569a891703a
Nicolai D. Madsen <Nico24dahl@gmail.com>
2023-09-25 15:30:51 +0200

    Deleted expansions cuase they fucked up

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         if(GameManager.gameState == GameState.AWAIT){
             GameManager.Start();
+            ExpansionManager.GenerateExpansions();
         }
         if(GameManager.gameState == GameState.STARTED){       
             GameManager.Initialize(2);
